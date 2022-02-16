5cf53820b56ea88ae9eea26661d4d8bf9156717e
adxiong <260245435@qq.com>
2022-02-16 21:46:54 +0800

    socket 修改

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -4,7 +4,7 @@
  * @Author: Adxiong
  * @Date: 2022-02-14 17:03:21
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-02-14 23:01:41
+ * @LastEditTime: 2022-02-15 15:51:01
  */
 
 
@@ -13,6 +13,9 @@ import * as io from 'socket.io'
 
 export default class SocketServer {
   socket: io.Server
+  userList: io.Socket[] = []
+  room: io.Socket[] = []
+
   
   constructor(server){
     this.socket =  new io.Server(server)
@@ -20,12 +23,9 @@ export default class SocketServer {
   }
   
   registerEventListen () {
-    this.socket.on('connect', (socket) => {
+    this.socket.on('connect', (socket) => {      
       socket.on('message', (data) => this.receiveMessage(socket,data))
-      socket.on('joinRoom', (data) => {
-        console.log(data);
-        
-      })
+      socket.on('joinRoom', (data) => this.joinRoom(socket, data))
       socket.on('disconnect', (data) => this.disconnect(socket,data))
     }) 
   }
@@ -36,12 +36,29 @@ export default class SocketServer {
 
   receiveMessage(socket, data) {
     return JSON.parse(data)
+
   }
 
-  joinRoom(socket, data) {
+  joinRoom(socket, context) {
     //给其他人推送，xx已经加入房间
-    console.log('joinRoom', data);
-    
+    const data = JSON.parse(context)    
+    socket["nickname"] = data.nickname
+    this.userList.push(socket)
+
+    //房间里查找有无此用户，没有就加入
+    const res = this.room.filter(client => client.id == socket.id)
+    if(!res.length) {
+      this.room.push(socket)
+      socket.join()
+
+      //给房间内所有人推送用户加入，不包括自己
+      for( const client of this.room) {
+        if (client.id == socket.id ) {
+          return
+        }
+        this.sendMessage(client, {message:`${socket['nickname']}加入房间`, type:"system"})
+      }
+    }
   }
 
   sendMessage(socket, data) {
