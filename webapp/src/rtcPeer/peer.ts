07ca13087d28352aa0290741bde2224c6cbd2cad
adxiong <260245435@qq.com>
2022-03-06 20:27:50 +0800

    update

--- webapp/src/rtcPeer/peer.ts
+++ webapp/src/rtcPeer/peer.ts
@@ -4,7 +4,7 @@
  * @Author: Adxiong
  * @Date: 2022-03-03 15:24:29
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-03-05 18:00:58
+ * @LastEditTime: 2022-03-06 16:56:15
  */
 
 import RTCPeer from "."
@@ -60,40 +60,31 @@ export default class Peer {
   }
 
   receiveAnswer(answer: RTCSessionDescription) {
-    this.peerConnection.setRemoteDescription(answer)
+    this.peerConnection.setRemoteDescription(new RTCSessionDescription(answer))
+  }
+
+  receiveIceCandidate(candidate: RTCIceCandidate) {
+    this.peerConnection.addIceCandidate(candidate)
   }
 
   replyAnswer(message: Message) {
-
-
-    
-  }
-
-  initPeerEvents () {
-    const pc = this.peerConnection
-    pc.addEventListener('track', (event: RTCTrackEvent ) => {
-      const stream = event.streams[0]
-
-      const setUserStream = () => {
-        if (!this.media.user || this.media.user.id != stream.id) {
-          this.media.user = stream
-  
-        }  
-      }
-      const setDisplayStram = () => {
-        if (!this.media.display || this.media.display.id != stream.id){
-          this.media.display = stream
+    if (message.type === 'answer') {
+      this.peerConnection.createAnswer()
+      .then( (answer) => 
+        this.peerConnection.setLocalDescription(answer)
+      )
+      .then( () => {
+        const sendMessage = {
+          type: "answer",
+          receiveId: message.userInfo.id,
+          payload: this.peerConnection.localDescription?.toJSON()
         }
-      }
-
-
-    })
-
-    pc.addEventListener('icecandidate', (event: RTCPeerConnectionIceEvent) => {
-      if ( event.cancelable) {
-        //发送ice
-      }
-    })
+        this.rtcPeerInstance.signalSend(sendMessage)
+      })
+      .catch( (error) => {
+        throw new Error(`replayAnswer error ==> ${error} `)
+      })
+    }
   }
 
   initDataChannelEvents (dc: RTCDataChannel) {
@@ -107,6 +98,73 @@ export default class Peer {
       
     }
   }
+
+
+
+
+  initPeerEvents () {
+    const pc = this.peerConnection
+
+    pc.addEventListener('icecandidate', (event: RTCPeerConnectionIceEvent) => {
+      if ( event.candidate) {
+        //发送ice
+        this.rtcPeerInstance.signalSend({
+          type: "icecandidate",
+          receiveId: this.id,
+          payload: event.candidate
+        })
+      }
+    })
+
+
+    pc.addEventListener('track', (event: RTCTrackEvent ) => {
+      const stream = event.streams[0]
+
+      const setUserStream = () => {
+        if (!this.media.user || this.media.user.id != stream.id) {
+          this.media.user = stream
+          this.rtcPeerInstance.emit('stream:user', this)
+        }  
+      }
+      const setDisplayStram = () => {
+        if (!this.media.display || this.media.display.id != stream.id){
+          this.media.display = stream
+          this.rtcPeerInstance.emit('stream:dispaly', this)
+        }
+      }
+
+      const streamType = trackType(stream)
+
+      if (streamType === 'user') setUserStream()
+      else if (streamType === 'display') setDisplayStram()
+
+    })
+
+    pc.addEventListener('negotiationneeded', () => {
+      pc.createOffer()
+      .then( offer => {
+        return pc.setLocalDescription(offer).then( () => offer)
+        }
+      )
+      .then( offer => {
+        this.rtcPeerInstance.emit('negotiationneeded:done', this)
+        this.rtcPeerInstance.signalSend({
+          type: 'offer',
+          receiveId: this.id,
+          payload: offer
+        })
+      })
+    })
+
+    pc.addEventListener('datachannel', (event) => {
+      const dc = event.channel
+      this.dataChannel = dc
+      this.isPeerConnected = true
+      this.rtcPeerInstance.emit('join', this)
+    })
+  }
+
+
 
 
 
